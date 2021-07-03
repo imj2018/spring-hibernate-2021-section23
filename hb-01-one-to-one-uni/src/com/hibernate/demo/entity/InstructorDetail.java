2bc084b984add6c105f429d98759991b7479b155
imj <imj.programming.2013@gmail.com>
2021-07-03 21:37:54 +1000

    213. @OneToOne - Write Some Code - Create Instructor class

--- hb-01-one-to-one-uni/src/com/hibernate/demo/entity/InstructorDetail.java
+++ hb-01-one-to-one-uni/src/com/hibernate/demo/entity/InstructorDetail.java
@@ -14,6 +14,7 @@ public class InstructorDetail {
 	// annotate class as an entity an map to db table
 	
 	// define fields
+	// annotate the fields with db column names
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="id")
@@ -25,6 +26,7 @@ public class InstructorDetail {
 	@Column(name="hobby")
 	private String hobby;
 	
+	// create constructors
 	public InstructorDetail() {
 		
 	}
@@ -34,7 +36,8 @@ public class InstructorDetail {
 		this.youtubeChannel = youtubeChannel;
 		this.hobby = hobby;
 	}
-
+	
+	// generate getter/setter methods
 	public int getId() {
 		return id;
 	}
@@ -58,7 +61,8 @@ public class InstructorDetail {
 	public void setHobby(String hobby) {
 		this.hobby = hobby;
 	}
-
+	
+	// generate toString() method
 	@Override
 	public String toString() {
 		return "InstructorDetail [id=" + id + ", youtubeChannel=" + youtubeChannel + ", hobby=" + hobby + "]";
@@ -66,13 +70,13 @@ public class InstructorDetail {
 	
 	
 	
-	// annotate the fields with db column names
+
 	
-	// create constructors
+
 	
-	// generate getter/setter methods
+
 	
-	// generate toString() method
+
 	
 	
 }
